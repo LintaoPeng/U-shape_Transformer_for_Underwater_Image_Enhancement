0b902d117a2288ef2d6e4f6b01b2ac73a40e4df4
Hassan-Tamer <hassantamerha@gmail.com>
2024-08-22 15:13:55 +0300

    fix skimage import error

--- net/utils.py
+++ net/utils.py
@@ -2,7 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import numpy as np
-from skimage.measure.simple_metrics import compare_psnr
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
 from torchvision import models
 
 
